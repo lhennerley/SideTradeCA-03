7d7a124241c29424a5b369a1b2ee48403df52374
Luke Hennerley <lhennerley@sidetrade.com>
2019-10-09 17:47:22 +0100

    Add the essential .NET Core projects for CA-03

---
+++ SideTradeCA.WebApi/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace SideTradeCA.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly string _connectionString;
+
+        public UsersController(IConfiguration configuration)
+        {
+            _connectionString = configuration["Postgres"];
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { _connectionString };
+        }
+    }
+}
